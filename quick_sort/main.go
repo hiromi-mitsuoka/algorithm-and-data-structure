cfac13db41b9ada8cda2d90da34e2948b2a35d8c
hiromi-mitsuoka <aehor8qe@gmail.com>
2022-04-22 17:27:36 +0900

    In the process of checking the sort package

--- quick_sort/main.go
+++ quick_sort/main.go
@@ -36,4 +36,67 @@ func main() {
 	// Use package
 	sort.Ints(nums)
 	fmt.Println(nums)
+
+	// slice := []int{2, 4, 3, 1, 5, 7, 6}
+	// fmt.Println(slice)
+	// fmt.Println(sort.IntSlice(slice))
 }
+
+// // ------------------------------------------------------
+// // Check sort package
+
+// func CheckInts(x []int) { CheckSort(CheckIntSlice(x))}
+
+// type CheckIntSlice []int
+
+// func (x CheckIntSlice) CheckSort() { CheckSort(x) }
+// func (x CheckIntSlice) Len() int { return(x) }
+
+// func CheckSort(data interface) {
+// 	n := data.Len()
+// 	CheckQuickSort(data, 0, n, CheckMaxDepth(n))
+// }
+
+// func CheckMaxDepth(){
+// 	var depth int
+// 	for i := n; i > 0; i >>= 1{
+// 		depth++
+// 	}
+// 	return depth*2
+// }
+
+// func CheckQuickSort(data interface, a, b, maxDepth int){
+// 	// Use ShellSort for slices <= 12 elements
+// 	for b - a > 12 {
+// 		if maxDepth == 0 {
+// 			heapSort(data, a, b)
+// 			return
+// 		}
+// 		maxDepth--
+// 		mlo, mhi := CheckDoPivot(data, a, b)
+// 		// Avoiding recursion on the larger subproblem guarantees
+// 		// a stack depth of at most lg(b-a)
+// 		if mlo-a < b-mhi {
+// 			CheckQuickSort(data, a, mlo, maxDepth)
+// 			a = mhi // i.e., CheckQuickSort(data, mhi, b)
+// 		} else {
+// 			CheckQuickSort(data, mhi, b, maxDepth)
+// 			b = mlo // i.e., CheckQuickSort(data, a, mlo)
+// 		}
+// 	}
+
+// 	// Do ShellSort pass with gap 6
+// 	// It could be written in this simplidied form cause b -a <= 12
+// 	for b - a > 1 {
+// 		for i:= a+6; i< b; i++ {
+// 			if data.Less(i, i-6) {
+// 				data.Swap(i, i-6)
+// 			}
+// 		}
+// 		insertionSort(data)
+// 	}
+// }
+
+// func CheckDoPivot(data interface, lo, hi int) (midlo, midhi int) {
+
+// }
